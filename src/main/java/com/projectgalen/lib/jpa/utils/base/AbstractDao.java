c964d31803875a61bd975d5962f225572ffc9d92
GalenRhodes <galenrhodes@me.com>
2023-04-10 16:16:50 -0400

    Initial Commit

--- src/main/java/com/projectgalen/lib/jpa/utils/base/AbstractDao.java
+++ src/main/java/com/projectgalen/lib/jpa/utils/base/AbstractDao.java
@@ -28,6 +28,9 @@ import com.projectgalen.lib.jpa.utils.errors.DaoException;
 import com.projectgalen.lib.jpa.utils.errors.SvcConstraintViolation;
 import com.projectgalen.lib.utils.PGProperties;
 import com.projectgalen.lib.utils.PGResourceBundle;
+import com.projectgalen.lib.utils.U;
+import com.projectgalen.lib.utils.reflection.Reflection;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PersistenceException;
 import org.hibernate.Hibernate;
 import org.hibernate.Session;
@@ -35,14 +38,14 @@ import org.hibernate.Transaction;
 import org.hibernate.exception.ConstraintViolationException;
 import org.hibernate.query.Query;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@SuppressWarnings({ "UnusedReturnValue", "unused" })
+@SuppressWarnings({ "UnusedReturnValue", "unused", "SameParameterValue" })
 public abstract class AbstractDao<T extends JpaBase> {
 
     protected static final PGProperties     props = PGProperties.getXMLProperties("settings.xml", HibernateUtil.class);
@@ -62,7 +65,66 @@ public abstract class AbstractDao<T extends JpaBase> {
         return getEntityClass().getSimpleName();
     }
 
-    public @NotNull T getNewEntity() {
+    /**
+     * Synonym for update(entity).
+     *
+     * @param entity The entity to insert into the database
+     * @param deep   If true it will also attempt to create/update any child objects as well.
+     */
+    protected void _create(@NotNull T entity, boolean deep) {
+        _update(entity, deep);
+    }
+
+    protected void _delete(@NotNull T entity) {
+        if(!entity.isNew()) {
+            try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+                _withTransaction(session, s -> s.remove(entity));
+                entity.setJpaState(JpaState.DELETED);
+            }
+        }
+    }
+
+    protected @NotNull List<T> _find(@NotNull String queryString, @Nullable Map<String, Object> parameters, boolean initializeResults) {
+        @NotNull List<T> list = Objects.requireNonNull(_findWithAction(queryString, parameters, q -> notNullResults(q.list())));
+        if(initializeResults) for(T entity : list) Hibernate.initialize(entity);
+        return list;
+    }
+
+    protected @NotNull List<T> _findAll(boolean initializeResults) {
+        return _find(String.format("from %s e", getEntityName()), null, initializeResults);
+    }
+
+    protected <R> @Nullable R _findWithAction(@NotNull String queryString, @Nullable Map<String, Object> parameters, @NotNull QueryAction<T, R> queryAction) {
+        return _withSessionGet(session -> {
+            Query<T> query = session.createQuery(queryString, getEntityClass());
+            if(parameters != null) for(Map.Entry<String, Object> entry : parameters.entrySet()) query.setParameter(entry.getKey(), entry.getValue());
+            return queryAction.action(query);
+        });
+    }
+
+    protected @Nullable T _get(String @NotNull [] searchFields, Object @NotNull [] searchValues) {
+        if(searchFields.length != searchValues.length) throw new IllegalArgumentException(msgs.format("msg.err.fields_values_count_mismatch", searchFields.length, searchValues.length));
+
+        Map<String, Object> params = new LinkedHashMap<>();
+        StringBuilder       sb     = new StringBuilder().append("from ").append(getEntityName()).append(" e where ");
+        boolean             first  = true;
+
+        for(int i = 0, j = searchFields.length; i < j; i++) {
+            String key = String.format("VAL%d", i);
+            if(first) first = false;
+            else sb.append(" and ");
+            sb.append("e.").append(searchFields[i]).append(" = :").append(key);
+            params.put(key, searchValues[i]);
+        }
+
+        return _findWithAction(sb.toString(), params, q -> init(q.uniqueResult()));
+    }
+
+    protected @Nullable T _get(@NotNull String searchField, @NotNull Object searchValue) {
+        return _get(U.asArray(searchField), U.asArray(searchValue));
+    }
+
+    protected @NotNull T _getNewEntity() {
         try {
             try {
                 return entityClass.getConstructor(boolean.class).newInstance(false);
@@ -76,115 +138,84 @@ public abstract class AbstractDao<T extends JpaBase> {
         }
     }
 
-    /**
-     * Synonym for update(entity).
-     *
-     * @param entity The entity to insert into the database
-     */
-    public void create(@NotNull T entity) {
-        update(entity);
-    }
-
-    public void delete(@NotNull T entity) {
-        if(!entity.isNew()) {
-            withSessionDo(session -> q(() -> session.remove(entity)));
-            entity.setJpaState(JpaState.DELETED);
-        }
-    }
-
-    public @NotNull List<T> findAll() {
+    protected void _update(@NotNull T entity, boolean deep) {
         try(Session session = HibernateUtil.getSessionFactory().openSession()) {
-            List<T> results = session.createQuery(props.format("pgbudget.dao.get_from", getEntityName()), getEntityClass()).list();
-            return ((results == null) ? Collections.emptyList() : results);
+            _withTransaction(session, s -> _update(s, entity, deep));
+            session.refresh(entity);
         }
     }
 
-    public T get(@NotNull Long id) {
-        return get("id", id);
-    }
-
-    public T get(long id) {
-        return get("id", id);
-    }
-
-    public T get(@NotNull String searchField, @NotNull Object searchValue) {
+    protected <R> R _withSessionGet(@NotNull SessionAction<R> delegate) {
         try(Session session = HibernateUtil.getSessionFactory().openSession()) {
-            String   valueKey = props.getProperty("pgbudget.dao.value_key");
-            Query<T> query    = session.createQuery(props.format("pgbudget.dao.get_unique", getEntityName(), searchField, valueKey), getEntityClass());
-            query.setParameter(valueKey, searchValue);
-            T result = query.uniqueResult();
-            Hibernate.initialize(result);
-            return result;
+            return delegate.action(session);
         }
     }
 
-    public void update(@NotNull T entity) {
+    protected void _withSessionUpdate(@NotNull SessionUpdateAction delegate) {
         try(Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            try {
-                switch(entity.getJpaState()) {
-                    case NEW:
-                    case DELETED:
-                        session.persist(entity);
-                        entity.setJpaState(JpaState.NORMAL);
-                        break;
-                    case DIRTY:
-                        session.merge(entity);
-                        entity.setJpaState(JpaState.NORMAL);
-                        break;
-                    default:
-                        break;
-                }
-                session.flush();
-                transaction.commit();
-                session.refresh(entity);
-            }
-            catch(Exception e) {
-                handleUpdateException(transaction, e);
-                throw e;
-            }
+            _withTransaction(session, delegate);
         }
     }
 
-    protected @NotNull T init(@NotNull T entity) {
-        Hibernate.initialize(entity);
+    protected @Nullable T init(@Nullable T entity) {
+        if(entity != null) Hibernate.initialize(entity);
         return entity;
     }
 
-    protected int q(@NotNull QDelegate qDelegate) {
-        qDelegate.action();
-        return 0;
-    }
-
-    protected <R> R withSessionDo(@NotNull SessionAction<R> delegate) {
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Transaction transaction = session.beginTransaction();
-            try {
-                R r = delegate.action(session);
-                session.flush();
-                transaction.commit();
-                return r;
-            }
-            catch(Exception e) {
-                handleUpdateException(transaction, e);
-                throw e;
-            }
+    private void _update(@NotNull Session session, @Nullable JpaBase entity, boolean deep) {
+        if(entity != null) {
+            if(deep) Reflection.forEachField(entity.getClass(), field -> {
+                if(field.isAnnotationPresent(ManyToOne.class) && JpaBase.class.isAssignableFrom(field.getType())) {
+                    try {
+                        field.setAccessible(true);
+                        _update(session, (JpaBase)field.get(entity), true);
+                    }
+                    catch(Exception e) {
+                        if(e instanceof RuntimeException) throw (RuntimeException)e;
+                        throw new DaoException(e);
+                    }
+                }
+                return false;
+            });
+            switch(entity.getJpaState()) {/*@f0*/
+                case NEW:
+                case DELETED: session.persist(entity); entity.setJpaState(JpaState.NORMAL); break;
+                case DIRTY:   session.merge(entity);   entity.setJpaState(JpaState.NORMAL); break;
+                default:                                                                    break;
+            }/*@f1*/
         }
     }
 
-    private static void handleUpdateException(Transaction transaction, Exception e) {
-        if(transaction != null) transaction.rollback();
-        if(((e instanceof PersistenceException) && (e.getCause() instanceof ConstraintViolationException) && (e.getCause().getCause() instanceof SQLIntegrityConstraintViolationException))) {
-            Matcher m = Pattern.compile(props.getProperty("pgbudget.dao.dup_key_regexp")).matcher(e.getCause().getCause().getMessage());
-            if(m.matches()) throw new SvcConstraintViolation(m.group(1), m.group(2), m.group(3), ((ConstraintViolationException)e.getCause()).getConstraintName());
+    private @NotNull List<T> notNullResults(@Nullable List<T> results) {
+        return (results == null) ? Collections.emptyList() : results;
+    }
+
+    protected static void _withTransaction(@NotNull Session session, @NotNull SessionUpdateAction delegate) {
+        Transaction transaction = session.beginTransaction();
+        try {
+            delegate.action(session);
+            session.flush();
+            transaction.commit();
+        }
+        catch(Exception e) {
+            transaction.rollback();
+            if(((e instanceof PersistenceException) && (e.getCause() instanceof ConstraintViolationException) && (e.getCause().getCause() instanceof SQLIntegrityConstraintViolationException))) {
+                Matcher m = Pattern.compile(props.getProperty("pgbudget.dao.dup_key_regexp")).matcher(e.getCause().getCause().getMessage());
+                if(m.matches()) throw new SvcConstraintViolation(m.group(1), m.group(2), m.group(3), ((ConstraintViolationException)e.getCause()).getConstraintName());
+            }
+            throw e;
         }
     }
 
-    protected interface QDelegate {
-        void action();
+    public interface QueryAction<O, R> {
+        R action(@NotNull Query<O> query);
     }
 
-    protected interface SessionAction<R> {
+    public interface SessionAction<R> {
         R action(@NotNull Session session);
+    }
+
+    public interface SessionUpdateAction {
+        void action(@NotNull Session session);
     }
 }
